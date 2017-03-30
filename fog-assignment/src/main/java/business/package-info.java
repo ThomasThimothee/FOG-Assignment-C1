1d50fc4aac7591150b2e24c9a218676f7601a1c8
Thomas Thimothee <thomasthimothee@admins-MacBook-Air.local>
2017-03-30 10:25:55 +0200

    add description of package info in business

--- fog-assignment/src/main/java/business/package-info.java
+++ fog-assignment/src/main/java/business/package-info.java
@@ -1,6 +1,9 @@
 /*
  * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ this package will be used to store the classes:
+    -   business logic class
+    -   facades that handle communication with db
+    -   servlets that handle communication with presentation
+    
  */
 package business;
