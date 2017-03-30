4158907a38f1b3844066c3ee96a6f998787983ef
mathiasjepsen <mathias@wpsnet.com>
2017-03-30 10:29:58 +0200

    Adjusted text in package info in business

--- fog-assignment/src/main/java/business/package-info.java
+++ fog-assignment/src/main/java/business/package-info.java
@@ -1,9 +1,10 @@
 /*
  * To change this license header, choose License Headers in Project Properties.
  this package will be used to store the classes:
+    - Remove this text later
     -   business logic class
     -   facades that handle communication with db
     -   servlets that handle communication with presentation
-    
+    - Added by Mathias as a test
  */
 package business;
--- fog-assignment/src/main/java/presentation/package-info.java
+++ fog-assignment/src/main/java/presentation/package-info.java
@@ -1,8 +1,7 @@
 /*
-
-This package will be used to store:
--   all the classes that have to do with the business logic 
--   Servlets that handle the communication with presentation layer
--   facades that handle communication with the db
- */
+    This package will be used to store:
+        -   all the classes that have to do with the business logic 
+        -   Servlets that handle the communication with presentation layer
+        -   facades that handle communication with the db
+*/
 package presentation;
