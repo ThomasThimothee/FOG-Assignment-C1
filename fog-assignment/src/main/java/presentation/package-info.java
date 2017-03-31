075530e7180ca8a960ee64001302aed4c17583b5
Thomas Thimothee <thomasthimothee@admins-MacBook-Air.local>
2017-03-31 08:47:24 +0200

    presentation package-info

--- fog-assignment/src/main/java/presentation/package-info.java
+++ fog-assignment/src/main/java/presentation/package-info.java
@@ -1,7 +1,5 @@
 /*
     This package will be used to store:
-        -   all the classes that have to do with the business logic 
-        -   Servlets that handle the communication with presentation layer
-        -   facades that handle communication with the db
+    -   used for front end
 */
 package presentation;
