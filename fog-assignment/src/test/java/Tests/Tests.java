7f438fdc0469b0be03fa2621ff6a3c57190ef7c0
mathiasjepsen <mathias@wpsnet.com>
2017-04-23 16:24:29 +0200

    Added JUnit test example

---
+++ fog-assignment/src/test/java/Tests/Tests.java
@@ -0,0 +1,48 @@
+package Tests;
+
+import business.Flat;
+import business.Partlist;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author mathiasjepsen
+ */
+public class Tests {
+    
+    Flat flat;
+    Partlist partlist;
+    
+    public Tests() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+        
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        flat = new Flat("Flat", "Plastmo Ecolite Blue", 250, 160, 300, 150, 100);
+        partlist = flat.createPartList();
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+
+    @Test
+    public void hello() {
+        assertEquals(360, partlist.getWoodParts().get(0).getLength());
+    }
+}
