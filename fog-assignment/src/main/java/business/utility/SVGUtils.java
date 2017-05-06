2f11e7f326c62e9a414e14532e2a8e6b69fe7304
mathiasjepsen <mathias@wpsnet.com>
2017-05-06 16:27:57 +0200

    Added drawPointyTopView method

--- fog-assignment/src/main/java/business/utility/SVGUtils.java
+++ fog-assignment/src/main/java/business/utility/SVGUtils.java
@@ -1,6 +1,8 @@
 package business.utility;
 
 import business.Carport;
+import business.Flat;
+import business.Pointy;
 
 /**
  *
@@ -27,15 +29,18 @@ public class SVGUtils {
         return " <rect x=\"" + x + "\" y=\"" + y + "\" width=\"" + width + "\" height=\"" + height + "\" style=\"fill:white;stroke:black;stroke-width:" + stroke + "\"/>";
     }
     
-    public String drawFlatTopView(Carport carport) {
-        double sWidth = carport.getShedWidth();
-        double cWidth = carport.getCarportWidth();
-        double tLength = carport.getTotalLength();
-        double cLength = carport.getCarportLength();
-        int spærtræQuantity = carport.getPartList().getPartList().get(0).getQuantity() - 1;
+    public String drawFlatTopView(Flat flat) {
+        double sWidth = flat.getShedWidth();
+        double cWidth = flat.getCarportWidth();
+        double tLength = flat.getTotalLength();
+        double cLength = flat.getCarportLength();
+        // Carport sides
+        String s = drawCarportSides(xOffset, yOffset, tLength, cWidth, 4);
+        
+        // 'Spær' 
+        int spærtræQuantity = flat.getPartList().getPartList().get(0).getQuantity() - 1;
         double initialSpærtræIncrement = tLength / spærtræQuantity;
         double spærtræIncrement = initialSpærtræIncrement;
-        String s = drawCarportSides(xOffset, yOffset, tLength, cWidth, 4);
         int i = 0;
         while (i < spærtræQuantity) {
             s += drawLine(spærtræIncrement, cWidth, spærtræIncrement, 0, 4);
@@ -106,4 +111,104 @@ public class SVGUtils {
         
         return s;
     }
+    
+    public String drawPointyTopView(Pointy pointy) {
+        double sWidth = pointy.getShedWidth();
+        double sLength = pointy.getShedLength();
+        double cWidth = pointy.getCarportWidth();
+        double tLength = pointy.getTotalLength();
+        double cLength = pointy.getCarportLength();
+        
+        // Carport sides
+        String s = drawCarportSides(xOffset, yOffset, tLength, cWidth, 4);
+        
+        // 'Spær'
+        int spærtræQuantity = pointy.getPartList().getPartList().get(0).getQuantity() - 1;
+        double initialSpærtræIncrement = cLength / spærtræQuantity;
+        double initialOffset = (cLength - (initialSpærtræIncrement / 2)) / spærtræQuantity;
+        double spærtræIncrement = initialOffset;
+        int i = 0;
+        while (i < spærtræQuantity) {
+            s += drawLine(spærtræIncrement - (initialOffset / 2), cWidth, spærtræIncrement - (initialOffset / 2), 0, 4);
+            spærtræIncrement += initialOffset;
+            i++;
+        }
+        s += drawLine(cLength, cWidth, cLength, 0, 4);
+        s += drawLine(cLength + ((sLength / 2) - (initialSpærtræIncrement / 4)), cWidth, cLength + ((sLength / 2) - (initialSpærtræIncrement / 4)), 0, 4);
+        s += drawLine(tLength - (initialSpærtræIncrement / 2), cWidth, tLength - (initialSpærtræIncrement / 2), 0, 4);
+        
+        // 'Remme' in the sides
+        s += drawLine(0 + (initialOffset / 2), (sWidth + ((cWidth - sWidth) / 2)), tLength - (initialOffset / 2), (sWidth + ((cWidth - sWidth) / 2)), 10);
+        s += drawLine(0 + (initialOffset / 2), ((cWidth - sWidth) / 2), tLength - (initialOffset / 2), ((cWidth - sWidth) / 2), 10);
+        
+        // Shed walls
+        s += drawLine(cLength, (sWidth + ((cWidth - sWidth) / 2)), cLength, ((cWidth - sWidth) / 2), 10);
+        s += drawLine(tLength - (initialSpærtræIncrement / 2), (cWidth - ((cWidth - sWidth) / 2)), tLength - (initialSpærtræIncrement / 2), ((cWidth - sWidth) / 2), 10);
+        
+        // 'Stolper'
+        double firstStolpeX = cLength * 0.18; 
+        double secondStolpeX = cLength * 0.78;
+        s += drawRect(firstStolpeX, ((cWidth - sWidth) / 2) - 4, 16, 16, 8);
+        s += drawRect(secondStolpeX, ((cWidth - sWidth) / 2) - 4, 16, 16, 8);
+        
+        s += drawRect(firstStolpeX, cWidth - ((cWidth - sWidth) / 2) - 4, 16, 16, 8);
+        s += drawRect(secondStolpeX, cWidth - ((cWidth - sWidth) / 2) - 4, 16, 16, 8);
+        
+        s += drawRect(cLength - 4, cWidth - ((cWidth - sWidth) / 2) - 4, 16, 16, 8);
+        s += drawRect(tLength - (initialSpærtræIncrement / 2) - 4, cWidth - ((cWidth - sWidth) / 2) - 4, 16, 16, 8);
+        
+        s += drawRect(cLength  - 4, ((cWidth - sWidth) / 2) - 4, 16, 16, 8);
+        s += drawRect(tLength - (initialSpærtræIncrement / 2) - 4, ((cWidth - sWidth) / 2) - 4, 16, 16, 8);
+        
+        // Horizontal beams
+        double initialBeamIncrement = cWidth / 8;
+        double beamIncrement = initialBeamIncrement;
+        i = 0;
+        while (i < 8) {
+            s += drawLine(0, beamIncrement, tLength, beamIncrement, 6);
+            beamIncrement += initialBeamIncrement;
+            i++;
+        }
+        s += drawLine(0, (cWidth / 2) - 6, tLength, (cWidth / 2) - 6, 6);
+        s += drawLine(0, (cWidth / 2) + 6, tLength, (cWidth / 2) + 6, 6);
+        
+        // Top dimensions
+        spærtræIncrement = initialOffset;
+        s += drawLine(0 + (initialOffset / 2), -10, 0 + (initialOffset / 2), -20, 2);
+        s += drawText(initialOffset, (initialOffset / 4) + (initialOffset / 2), -10, "");
+        i = 0;
+        while (i < spærtræQuantity) {
+            s += drawLine(spærtræIncrement + (initialOffset / 2), -10, spærtræIncrement + (initialOffset / 2), -20, 2);
+            if (i < spærtræQuantity - 1) {
+                s += drawText(initialOffset, ((initialOffset / 4) + (initialOffset / 2) + spærtræIncrement), -10, "");
+            }
+            spærtræIncrement += initialOffset;
+            i++;
+        }
+        s += drawLine(cLength + ((sLength / 2) - (initialSpærtræIncrement / 4)), -10, cLength + ((sLength / 2) - (initialSpærtræIncrement / 4)), -20, 2);
+        s += drawLine(tLength - (initialSpærtræIncrement / 2), -10, tLength - (initialSpærtræIncrement / 2), -20, 4);
+        s += drawText((sLength / 2), cLength + ((sLength / 4) - (initialSpærtræIncrement / 4)), -10, "");
+        s += drawText((sLength / 2), cLength + ((sLength / 2) + ((sLength / 8)) - (initialSpærtræIncrement / 4)), -10, "");
+        
+        // Bottom dimensions
+        s += drawLine(0, cWidth + 30, 0, cWidth + 20, 2);
+        s += drawLine(tLength, cWidth + 30, tLength, cWidth + 20, 2);
+        s += drawLine(0, cWidth + 30, tLength, cWidth + 30, 2);
+        s += drawText(tLength, (tLength / 2), cWidth + 20, "");
+        
+        // Left outer dimensions
+        s += drawLine(-10, 0, -70, 0, 2);
+        s += drawLine(-10, cWidth, -70, cWidth, 2);
+        s += drawLine(-70, 0, -70, cWidth, 2);
+        s += drawText(cWidth, (-(xOffset * 2) + (xOffset - yOffset)) - (cWidth / 2), -10, "rotate(-90, 0, 0)");
+        
+        // Left inner dimensions
+        s += drawLine(-10, ((cWidth - sWidth) / 2), -40, ((cWidth - sWidth) / 2), 2);
+        s += drawLine(-10, cWidth - ((cWidth - sWidth) / 2), -40, cWidth - ((cWidth - sWidth) / 2), 2);
+        s += drawLine(-40, ((cWidth - sWidth) / 2), -40, cWidth - ((cWidth - sWidth) / 2), 2);
+        s += drawText(sWidth, (-(xOffset * 2) + (xOffset - yOffset)) - (cWidth / 2), 20, "rotate(-90, 0, 0)");
+        
+        return s;
+    }
+    
 }
--- fog-assignment/src/main/webapp/TestSVGUtils.jsp
+++ fog-assignment/src/main/webapp/TestSVGUtils.jsp
@@ -1,3 +1,4 @@
+<%@page import="business.Pointy"%>
 <%@page import="business.Flat"%>
 <%@page import="business.parts.Wood"%>
 <%@page import="business.parts.Part.PartType"%>
@@ -10,16 +11,24 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     </head>
     <body>
-        <% Flat flat = new Flat("Flat", "Plastmo Ecolite Blue", 240, 240, 150, 210, 0);
+        <%  Flat flat = new Flat("Flat", "Plastmo Ecolite Blue", 570, 600, 210, 540, 0);
+            Pointy pointy = new Pointy("Pointy", "Plastmo Ecolite Blue", 510, 360, 210, 330, 0, 30);
             SVGUtils svg = new SVGUtils();
-            Partlist partlist = new Partlist();
-            int nb = (int) ((flat.getTotalLength() / 55) + 1);
-            Wood woodJ = new Wood(PartType.WOOD, nb, 480, "ubh. spærtræ", "spær. monteres på rem");;
-            partlist.getPartList().add(0, woodJ);
-            flat.setPartList(partlist);%>
+            Partlist partlistf = new Partlist();
+            Partlist partlistp = new Partlist();
+            int nbf = (int) ((flat.getTotalLength() / 55) + 1);
+            int nbp = (int) ((pointy.getCarportLength() / 55) + 1);
+            Wood woodJf = new Wood(PartType.WOOD, nbf, 480, "ubh. spærtræ", "spær. monteres på rem");
+            Wood woodJp = new Wood(PartType.WOOD, nbp, 480, "ubh. spærtræ", "spær. monteres på rem");
+            partlistf.getPartList().add(0, woodJf);
+            partlistp.getPartList().add(0, woodJp);
+            flat.setPartList(partlistf);
+            pointy.setPartList(partlistp);%>
         <svg viewBox="0, 0, <%=flat.getTotalLength() + 100%>, <%=flat.getCarportWidth() + 100%>" width="1024" height="768" preserveAspectRatio="xMinYMin meet">
-        <%=svg.drawFlatTopView(flat)%>
-        
+            <%=svg.drawFlatTopView(flat)%>
+        </svg>
+        <svg viewBox="0, 0, <%=pointy.getTotalLength() + 100%>, <%=pointy.getCarportWidth() + 100%>" width="1024" height="768" preserveAspectRatio="xMinYMin meet">
+            <%=svg.drawPointyTopView(pointy)%>
         </svg>
     </body>
 </html>
