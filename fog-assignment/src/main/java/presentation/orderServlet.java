684178ccc14f0610ccc630148317b2198eed8f50
mathiasjepsen <mathias@wpsnet.com>
2017-05-09 14:05:34 +0200

    Added setContentType and setCharacterEncoding to servlet

--- fog-assignment/src/main/java/presentation/orderServlet.java
+++ fog-assignment/src/main/java/presentation/orderServlet.java
@@ -25,6 +25,7 @@ public class orderServlet extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
+        request.setCharacterEncoding("UTF-8");
         HttpSession session = request.getSession();
         Customer customer = (Customer) session.getAttribute("currentCustomer");// try to retrieve current user
         String action = request.getParameter("action");
