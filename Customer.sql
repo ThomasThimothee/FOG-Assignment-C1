dce261d8fe9b6189ce6a19cb564655d6a278254e
Thomas Thimothee <thomasthimothee@admins-MacBook-Air.local>
2017-05-26 10:42:49 +0200

    script tables

---
+++ Customer.sql
@@ -0,0 +1,14 @@
+/*
+-- Query: SELECT * FROM fog.Customer
+LIMIT 0, 1000
+
+-- Date: 2017-05-26 10:37
+*/
+INSERT INTO `Customer` (`idCustomer`,`email`,`password`,`firstName`,`lastName`,`address`,`phone`) VALUES (3,'thom@mail.com','1234567','Thomas','Thimothee','235 in second avenue','425-546-6346');
+INSERT INTO `Customer` (`idCustomer`,`email`,`password`,`firstName`,`lastName`,`address`,`phone`) VALUES (4,'mathias@wpsnet.com','1234567','Mathias','Jepsen','Kongevejen 438','30664636');
+INSERT INTO `Customer` (`idCustomer`,`email`,`password`,`firstName`,`lastName`,`address`,`phone`) VALUES (5,'lovro@mail.com','1234567','Lovro','Bovro','53264 road ','21346486');
+INSERT INTO `Customer` (`idCustomer`,`email`,`password`,`firstName`,`lastName`,`address`,`phone`) VALUES (7,'test@test.com','1234567','Tester','Testerton','Testerstreet 139','90490302');
+INSERT INTO `Customer` (`idCustomer`,`email`,`password`,`firstName`,`lastName`,`address`,`phone`) VALUES (8,'mathias@mail.com','1234567','','','','');
+INSERT INTO `Customer` (`idCustomer`,`email`,`password`,`firstName`,`lastName`,`address`,`phone`) VALUES (9,'kri@mail.com','kri1234','Kri','KriH','camilla 4532','21453452');
+INSERT INTO `Customer` (`idCustomer`,`email`,`password`,`firstName`,`lastName`,`address`,`phone`) VALUES (10,'mathias@mail2.com','1234567','','','','');
+INSERT INTO `Customer` (`idCustomer`,`email`,`password`,`firstName`,`lastName`,`address`,`phone`) VALUES (11,'antonio@antonio.com','12345678','Antonio','Antonio','Lel 12','21544236634');
---
+++ SalesRep.sql
@@ -0,0 +1,9 @@
+/*
+-- Query: SELECT * FROM fog.SalesRep
+LIMIT 0, 1000
+
+-- Date: 2017-05-26 10:41
+*/
+INSERT INTO `SalesRep` (`idSalesRep`,`userName`,`password`,`firstName`,`lastName`,`phone`,`email`) VALUES (1,'KasperK','1234567','Kasper','Koe','24532-2352','kasper@mail.com');
+INSERT INTO `SalesRep` (`idSalesRep`,`userName`,`password`,`firstName`,`lastName`,`phone`,`email`) VALUES (2,'LovroE','1234567','Lovro','LEmployee','1224235-64643','lovroe@mail.com');
+INSERT INTO `SalesRep` (`idSalesRep`,`userName`,`password`,`firstName`,`lastName`,`phone`,`email`) VALUES (3,'ThomasE','1111111','Thomas','TEmployee','1523643-35754','thomase@mail.com');
