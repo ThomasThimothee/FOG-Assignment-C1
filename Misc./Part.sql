48838dc4d49ee7c14fc2954da684c276d8b320da
mathiasjepsen <mathias@wpsnet.com>
2017-08-15 16:10:54 +0200

    Moved some files to misc folder

---
+++ Misc./Part.sql
@@ -0,0 +1,37 @@
+
+
+
+INSERT INTO `Part` (`type`,`name`,`standardPrice`) VALUES ('SCREW','10x120 mm. Braeddebolt',20);
+INSERT INTO `Part` (`type`,`name`,`standardPrice`) VALUES ('WOOD','19x100 mm. trykimp. Braet',5);
+INSERT INTO `Part` (`type`,`name`,`standardPrice`) VALUES ('SCREW','1x20 mm. hulband 10 mtr.',100);
+INSERT INTO `Part` (`type`,`name`,`standardPrice`) VALUES ('WOOD','25x125 mm. trykimp. Braet',9);
+INSERT INTO `Part` (`type`,`name`,`standardPrice`) VALUES ('WOOD','25x150 mm. trykimp. Braet',3);
+INSERT INTO `Part` (`type`,`name`,`standardPrice`) VALUES ('WOOD','25x200 mm. trykimp. Braet',2);
+INSERT INTO `Part` (`type`,`name`,`standardPrice`) VALUES ('WOOD','25x50 mm. trykimp. Braet',5);
+INSERT INTO `Part` (`type`,`name`,`standardPrice`) VALUES ('WOOD','38x73 mm. Laegte',6);
+INSERT INTO `Part` (`type`,`name`,`standardPrice`) VALUES ('WOOD','38x73 mm. Taglaegte T1',6);
+INSERT INTO `Part` (`type`,`name`,`standardPrice`) VALUES ('SCREW','4,5x50 mm. skruer 300 stk.',300);
+INSERT INTO `Part` (`type`,`name`,`standardPrice`) VALUES ('SCREW','4,5x50 mm. skruer 350 stk.',350);
+INSERT INTO `Part` (`type`,`name`,`standardPrice`) VALUES ('SCREW','4,5x60 mm. skruer 200 stk.',200);
+INSERT INTO `Part` (`type`,`name`,`standardPrice`) VALUES ('SCREW','4,5x70 mm. skruer 200 stk.',200);
+INSERT INTO `Part` (`type`,`name`,`standardPrice`) VALUES ('SCREW','4,5x70 mm. skruer 400 stk.',400);
+INSERT INTO `Part` (`type`,`name`,`standardPrice`) VALUES ('SCREW','40x40 mm. firkantskiver',200);
+INSERT INTO `Part` (`type`,`name`,`standardPrice`) VALUES ('WOOD','45x195 mm. ubh. Spaertrae',4);
+INSERT INTO `Part` (`type`,`name`,`standardPrice`) VALUES ('WOOD','45x95 mm. ubh. Reglar',6);
+INSERT INTO `Part` (`type`,`name`,`standardPrice`) VALUES ('SCREW','4x50 mm. beslagskruer 250 stk.',250);
+INSERT INTO `Part` (`type`,`name`,`standardPrice`) VALUES ('SCREW','50x75 mm. Stalddorsgreb',200);
+INSERT INTO `Part` (`type`,`name`,`standardPrice`) VALUES ('SCREW','5x100 mm. skruer 100 stk.',100);
+INSERT INTO `Part` (`type`,`name`,`standardPrice`) VALUES ('SCREW','5x40 mm. beslagskruer 250 stk.',250);
+INSERT INTO `Part` (`type`,`name`,`standardPrice`) VALUES ('WOOD','97x97 mm. trykimp. Stolpe',3);
+INSERT INTO `Part` (`type`,`name`,`standardPrice`) VALUES ('TILE','B&C Dobbelt -s sort',2);
+INSERT INTO `Part` (`type`,`name`,`standardPrice`) VALUES ('TILE','B&C Rygsten sort',5);
+INSERT INTO `Part` (`type`,`name`,`standardPrice`) VALUES ('TILE','B&C Rygstensbeslag',9);
+INSERT INTO `Part` (`type`,`name`,`standardPrice`) VALUES ('TILE','B&C Tagstensbindere & nakkekroge',5);
+INSERT INTO `Part` (`type`,`name`,`standardPrice`) VALUES ('TILE','B&C Toplaegte holder',3);
+INSERT INTO `Part` (`type`,`name`,`standardPrice`) VALUES ('WOOD','faedigskaret',4);
+INSERT INTO `Part` (`type`,`name`,`standardPrice`) VALUES ('SCREW','Plastmo bundskruer 200 stk.',200);
+INSERT INTO `Part` (`type`,`name`,`standardPrice`) VALUES ('WOOD','Plastmo Ecolite Blue',6);
+INSERT INTO `Part` (`type`,`name`,`standardPrice`) VALUES ('SCREW','t-haengsel',300);
+INSERT INTO `Part` (`type`,`name`,`standardPrice`) VALUES ('SCREW','universal hojre',10);
+INSERT INTO `Part` (`type`,`name`,`standardPrice`) VALUES ('SCREW','universal venstre',10);
+INSERT INTO `Part` (`type`,`name`,`standardPrice`) VALUES ('SCREW','vinkelbeslag',30);
